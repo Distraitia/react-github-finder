c39c1b3b99be8e05acc5eca673a1589a3ceddc3f
Allison Lewis <alewis@neboagency.com>
2019-12-08 15:54:37 -0500

    Code at end of Section 2-9

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  otherFoo = () => 'BUR';
   render() {
+    const name = 'Jane Doe';
+    const foo = () => 'Bar';
+    const loading = false;
+    const showName = false;
+
     return (
       <div className='App'>
-        <h1>Hello from React</h1>
-        <h2>Goodbye</h2>
+        <h1>Hello {showName && name}</h1>
+        <h2>This is {foo()}</h2>
+        <h2>And this is {this.otherFoo()}</h2>
+        {loading ? <h4>Loading...</h4> : <h4>Loaded!</h4>}
       </div>
     );
   }
