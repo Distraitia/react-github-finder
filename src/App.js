74c85d71fcdeff306abc1c7fd8915327fe0aeda5
Allison Lewis <alewis@neboagency.com>
2019-12-08 16:41:27 -0500

    Add Navbar componeent with default properties and PropTypes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
-  otherFoo = () => 'BUR';
   render() {
-    const name = 'Jane Doe';
-    const foo = () => 'Bar';
-    const loading = false;
-    const showName = false;
-
     return (
       <div className='App'>
-        <h1>Hello {showName && name}</h1>
-        <h2>This is {foo()}</h2>
-        <h2>And this is {this.otherFoo()}</h2>
-        {loading ? <h4>Loading...</h4> : <h4>Loaded!</h4>}
+        <Navbar title='Github Finder' icon='fab fa-github' />
       </div>
     );
   }
